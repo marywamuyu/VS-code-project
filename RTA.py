3bddf66ecbd66daa16f869efd1e92d67085f31c0
mary <ceciliamaina203@gmail.com>
2023-10-30 12:17:40 +0300

    vs project

--- RTA.py
+++ RTA.py
@@ -4,9 +4,9 @@ import plotly.express as px
 import streamlit as st
 
 # Title 
-st.title("Road Traffic Accidents")
+st.title("Analysis of Road Traffic Accidents")
 #introduction
-st.write("The RTA is to show the accidedents that occur, major causes, build models that predict injury severity of injured accidents, the road conditions monitoring and by data visualization ,creating dashboards and reports to present accident data in a visually accessible maner to help in informed decisions.")
+st.write("The RTA is to show the accidents that occur, major causes, build models that predict injury severity , the road conditions monitoring and by data visualization ,creating dashboards and reports to present accident data in a visually accessible maner to help in informed decisions,it is also used in predicting the impact of drivers age on accidents and accident forecast by weather conditions.")
 #displaying image
 st.write("Displaying an image:")
 st.image("road accident image.jpg", caption="Road traffic accidents remains a pervasive and critical issue worldwide.This accidents leads to significant loss of life, injuries, and economic coast affecting individuals and the nation.This project enhancing road saftety seeks to address this pressing concern by conducting a comprehensive analysis of road traffic accidents", use_column_width=True)
@@ -34,59 +34,94 @@ def load_data():
     return data
 
 data = load_data()  
-# Specify the format for the 'Time' column
-data['Time'] = pd.to_datetime(data['Time'], format='%H:%M:%S')
-
-# Create a new column 'Time_of_day' as a formatted string
-data['Time_of_day'] = data['Time'].dt.strftime('%H:%M')
-
-# Streamlit app
-st.write("Data loaded successfully!")
 
 #displaying the data
 if st.checkbox("Show Raw Data"):
     st.subheader("Raw Data")
     st.write(data)
     #creating the visualizations
-# Bar plot for Day_of_week
-day_of_week_counts = data['Day_of_week'].value_counts().reset_index()
-day_of_week_counts.columns = ['Day_of_week', 'Count']
-
-fig = px.bar(day_of_week_counts, x='Day_of_week', y='Count', title='Accidents by Day of the Week')
-fig.update_xaxes(categoryorder='total ascending')
-fig.update_xaxes(tickangle=45)
-fig.show()
 # Pie chart for Accident Severity
 accident_severity_counts = data['Accident_severity'].value_counts().reset_index()
 accident_severity_counts.columns = ['Accident_severity', 'Count']
 
 fig = px.pie(accident_severity_counts, names='Accident_severity', title='Distribution of Accident Severity')
-fig.show()
+st.plotly_chart(fig)
+
 # Create a scatter plot
 fig = px.scatter(data, x='Age_band_of_driver', y='Number_of_casualties', color='Accident_severity',
                  title='Age of Driver vs. Number of Casualties by Accident Severity')
 fig.update_xaxes(title='Age of Driver')
 fig.update_yaxes(title='Number of Casualties')
-fig.show()
+st.plotly_chart(fig)
 # Box plot for Age Distribution by Accident Severity
 fig = px.box(data, x='Accident_severity', y='Age_band_of_driver', color='Accident_severity',
              title='Age Distribution by Accident Severity')
 fig.update_xaxes(title='Accident Severity')
 fig.update_yaxes(title='Age of Driver')
-fig.show()
+st.plotly_chart(fig)
 # Violin plot for Age Distribution by Accident Severity
 fig = px.violin(data, x='Accident_severity', y='Age_band_of_driver', color='Accident_severity',
                title='Age Distribution by Accident Severity (Violin Plot)')
 fig.update_xaxes(title='Accident Severity')
 fig.update_yaxes(title='Age of Driver')
-fig.show()
+st.plotly_chart(fig)
 # Scatter plot for Light Conditions vs. Weather Conditions
 fig = px.scatter(data, x='Light_conditions', y='Weather_conditions', color='Accident_severity',
                  title='Light Conditions vs. Weather Conditions by Accident Severity')
 fig.update_xaxes(title='Light Conditions')
 fig.update_yaxes(title='Weather Conditions')
-fig.show()
+st.plotly_chart(fig)
 
+# Title 
+st.title('Impact of Driver Age on Accidents')
+
+# Sidebar for user input
+st.sidebar.subheader('Select Age Band of Driver')
+age_band = st.sidebar.selectbox('Select Age Band', data['Age_band_of_driver'].unique())
+
+# Filter data based on user-selected age band
+filtered_data = data[data['Age_band_of_driver'] == age_band]
+
+# Display insights or predictions based on the selected age band
+st.subheader('Insights for the Selected Age Band')
+if filtered_data.empty:
+    st.write(f'No data available for the selected age band: {age_band}')
+else:
+    # Create visualizations or perform data analysis
+    #  create a bar plot of accident severity for the selected age band.
+    accident_severity_counts = filtered_data['Accident_severity'].value_counts().reset_index()
+    accident_severity_counts.columns = ['Accident_severity', 'Count']
+    fig = px.bar(accident_severity_counts, x='Accident_severity', y='Count', title=f'Accident Severity for {age_band}')
+    st.plotly_chart(fig)
+# Title 
+st.title('Accidents Forecast by Weather Condition')
+
+# Sidebar for user input
+st.sidebar.subheader('Select Weather Condition')
+selected_weather_condition = st.sidebar.selectbox('Select Weather Condition', data['Weather_conditions'].unique())
+
+# Filter data based on user-selected weather condition
+filtered_data = data[data['Weather_conditions'] == selected_weather_condition]
+
+# Display insights or predictions based on the selected weather condition
+st.subheader(f'Insights for {selected_weather_condition} Weather Condition')
+if filtered_data.empty:
+    st.write(f'No data available for the selected weather condition: {selected_weather_condition}')
+else:
+    # Perform data analysis or provide insights
+    # calculate statistics or trends related to accident severity for the selected weather condition.
+    accident_severity_counts = filtered_data['Accident_severity'].value_counts().reset_index()
+    accident_severity_counts.columns = ['Accident_severity', 'Count']
+    st.write('Accident Severity by Weather Condition:')
+    st.write(accident_severity_counts)
+    
+# Create a bar plot for road conditions using Plotly
+road_condition_counts = data['Road_surface_conditions'].value_counts().reset_index()
+road_condition_counts.columns = ['Road_surface_conditions', 'Count']
+
+st.subheader('Road Conditions Distribution')
+fig = px.bar(road_condition_counts, x='Road_surface_conditions', y='Count', title='Distribution of Road Conditions')
+st.plotly_chart(fig)
 
 
 
